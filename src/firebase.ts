30da2094a28c27e7db23fb80e5f8fe13e61d084d
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-05-23 03:22:32 -0400

    sdfs

--- src/firebase.ts
+++ src/firebase.ts
@@ -31,16 +31,16 @@ export const auth = getAuth(app);
 export const database = getDatabase(app);
 
 // New Medford Family Firebase configuration
-// const firebaseConfigMedfordFamily = {
-//   apiKey: "AIzaSyAq6iz-1HFHk6EKxHkdt8c_2suJ91jJ5N8",
-//   authDomain: "hospital-uid-medfordfamily.firebaseapp.com",
-//   databaseURL: "https://hospital-uid-medfordfamily-default-rtdb.firebaseio.com",
-//   projectId: "hospital-uid-medfordfamily",
-//   storageBucket: "hospital-uid-medfordfamily.firebasestorage.app",
-//   messagingSenderId: "912435094498",
-//   appId: "1:912435094498:web:6f6afbdb4608b77ebf0fbb",
-//   measurementId: "G-V6B2N49YZ8"
-// };
+const firebaseConfigMedfordFamily = {
+  apiKey: "AIzaSyAq6iz-1HFHk6EKxHkdt8c_2suJ91jJ5N8",
+  authDomain: "hospital-uid-medfordfamily.firebaseapp.com",
+  databaseURL: "https://hospital-uid-medfordfamily-default-rtdb.firebaseio.com",
+  projectId: "hospital-uid-medfordfamily",
+  storageBucket: "hospital-uid-medfordfamily.firebasestorage.app",
+  messagingSenderId: "912435094498",
+  appId: "1:912435094498:web:6f6afbdb4608b77ebf0fbb",
+  measurementId: "G-V6B2N49YZ8"
+};
 
 // Initialize new Firebase app with a different name ("medfordFamily")
 const medfordFamilyApp = initializeApp(firebaseConfigMedfordFamily, "medfordFamily");
